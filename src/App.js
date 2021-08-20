91fedd562905254aeb0bfaf657c25f0b9ed40b95
Sawyer <sawyer.pearson92@gmail.com>
2021-08-19 21:47:16 -0600

    base router installed and routes user to login page when no bearer token is present

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import TileContainer from './components/TileContainer';
 import Login from './components/Login';
 import './App.css';
 import Register from './components/Register';
+import {Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="app">
-      <Login/>
-      <Register/>
-      {/* <TileContainer/> */}
-    </div>
-  );
+  return (<>
+    {localStorage.getItem('bearer-token') ? (
+      <p>test</p>
+    ) : (
+      <p>blank</p>
+    )}
+
+   
+  </>);
 }
 
 export default App;

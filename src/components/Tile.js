12c033eabd2203d67daa7c3a5297e02f65a92d26
Sawyer <sawyer.pearson92@gmail.com>
2021-08-22 17:57:10 -0600

    put functionality established for cards. need to force a rerender and update state still.

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,13 @@
 import {useState} from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import {Form, FormGroup, Button, Label, Input} from 'reactstrap';
 
 const Tile = (props) => {
     const [tileInfo, setTileInfo] = useState(props)
+    const [updatedTile, setUpdatedTile] = useState({
+        can_name: '',
+        can_text: ''
+    })
 
     const handleCopy = () => {
         // setTileInfo(null)
@@ -17,16 +22,49 @@ const Tile = (props) => {
         })
     }
 
+    const updateCan = (e) => {
+        e.preventDefault();
+
+        axiosWithAuth()
+        .put(`https://cans-be.herokuapp.com/api/cans/update-can/${props.tileData.id}`, updatedTile)
+        .then((res) => {
+            console.log('res from editCan put', res)
+        })
+    }
+
+    const handleChanges = (e) => {
+        e.persist();
+        const currentTile = {
+            ...updatedTile,
+            [e.target.name]: e.target.value
+        }
+        setUpdatedTile(currentTile) 
+    }
+
     return (<> 
         <div className='tile'>
             <div>
                 <span onClick={() => handleCopy()}>|C|</span>
-                <span> |*| </span>
+                <span onClick={() => updateCan()}> |*| </span>
                 <span onClick={() => deleteCan()}>|X|</span>
             </div>
             {props.tileData.can_name}
             <div className='tile-text'>{props.tileData.can_text}</div>
         </div>
+
+        <Form onSubmit={updateCan}>
+                <FormGroup>
+                    <Label for="can_name"></Label>
+                    <Input type="text" name="can_name" placeholder="Can Name" onChange={handleChanges} value={updatedTile.can_name} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="can_text"></Label>
+                    <Input type="text" name="can_text" placeholder="Can Text" onChange={handleChanges} value={updatedTile.can_text} />
+                </FormGroup>
+
+            
+                <Button type="submit">Update</Button>
+        </Form>
     </>)
 }
 

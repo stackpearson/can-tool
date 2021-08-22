6fd0b84794c1a59815258db80b013e811991dc5a
Sawyer <sawyer.pearson92@gmail.com>
2021-08-22 17:31:15 -0600

    updated addCans function so it posts correctly. still need to figure out why page is not rerendering though.

--- src/components/TileContainer.js
+++ src/components/TileContainer.js
@@ -1,10 +1,10 @@
 import Tile from './Tile';
 import {useEffect, useState} from 'react';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import axios from 'axios';
+import {Form, FormGroup, Button, Label, Input} from 'reactstrap';
 
 const TileContainer = (props) => {
-
-    // let user_id = localStorage.getItem('user-id');
 
     useEffect(() => {
         getCans();
@@ -19,9 +19,70 @@ const TileContainer = (props) => {
         })
     }
 
+    // const addCan = (e) => {
+    //     e.preventDefault();
+    //     e.persist();
+
+    //     axios
+    //     console.log('addCan fired')
+    //     console.log('newTile', newTile)
+    //     // console.log('newTile:', newTile)
+    //     .post(`https://cans-be.herokuapp.com/api/cans/new-can/${localStorage.getItem('user-id')}`, newTile)
+    //     .then((res) => {
+    //         console.log("this is from the addCan post", res.data)
+    //         tileInfo.push(res.data)
+    //     })
+    // }
+
+    const addCan = (e) => {
+        e.preventDefault();
+        e.persist();
+
+        axiosWithAuth()
+        .post(`https://cans-be.herokuapp.com/api/cans/new-can/${localStorage.getItem('user-id')}`, newTile)
+        .then((res) => {
+            console.log('res from addCan post', res.data)
+            tileInfo.push(res.data)
+
+        })
+    }
+
+    const handleChanges = (e) => {
+        e.persist();
+        const currentTile = {
+            ...newTile,
+            [e.target.name]: e.target.value
+        }
+        setNewTile(currentTile) 
+    }
+
 
     const [tileInfo, setTileInfo] = useState([])
+
+    const [newTile, setNewTile] = useState({
+        can_name: '',
+        can_text: ''
+    })
+
+
+
     return(<>
+    <div>
+        <span>Add Can</span>
+    </div>
+    <Form onSubmit={addCan}>
+                <FormGroup>
+                    <Label for="can_name"></Label>
+                    <Input type="text" name="can_name" placeholder="Can Name" onChange={handleChanges} value={newTile.can_name} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="can_text"></Label>
+                    <Input type="text" name="can_text" placeholder="Can Text" onChange={handleChanges} value={newTile.can_text} />
+                </FormGroup>
+
+            
+                <Button type="submit">Add</Button>
+                </Form>
         <div className='tile-container'>
         {tileInfo.map((tile) => (
             <Tile key={tile.id} tileData={tile} />

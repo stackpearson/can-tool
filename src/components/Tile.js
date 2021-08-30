a7e7cdaeb9ad2fd36344a825189eed55c236df6d
Sawyer <sawyer.pearson92@gmail.com>
2021-08-29 21:29:09 -0600

    icons added for tiles

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,9 @@
 import {useState} from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
-import {Form, FormGroup, Button, Label, Input} from 'reactstrap';
+import {Form, FormGroup, Button, Label, Input, UncontrolledTooltip} from 'reactstrap';
 import {connect} from 'react-redux';
 import {updateCan, deleteCan} from '../actions/canActions';
+import {BsFillGearFill, BsClipboard, BsTrash, BsFillEyeFill, BsFillEyeSlashFill} from 'react-icons/bs';
 
 const Tile = (props) => {
     const [updatedTile, setUpdatedTile] = useState({
@@ -45,15 +46,84 @@ const Tile = (props) => {
         setUpdatedTile(currentTile) 
     }
 
+    // const showCanText = () => {
+    //     if (document.getElementById('can-text') && document.getElementById('view-icon') && document.getElementById('hide-icon-container')) {
+    //       let form = document.getElementById('can-text');
+    //       let showIcon = document.getElementById('view-icon')
+    //       let hideIcon = document.getElementById('hide-icon-container')
+    //       form.classList.remove('hidden')
+    //       showIcon.classList.add('hidden')
+    //       hideIcon.classList.remove('hidden')
+    //     }
+    //   }
+
+    //   const hideCanText = () => {
+    //     if (document.getElementById('can-text') && document.getElementById('show-icon-container') && document.getElementById('hide-icon-container')) {
+    //       let form = document.getElementById('can-text');
+    //       let showIcon = document.getElementById('show-icon-container')
+    //       let hideIcon = document.getElementById('hide-icon-container')
+    //       form.classList.add('hidden')
+    //       showIcon.classList.remove('hidden')
+    //       hideIcon.classList.add('hidden')
+    //     }
+    //   }
+
+    const showCanText = () => {
+        if (document.getElementById('can-text') && document.getElementById('view-icon') && document.getElementById('hide-icon')) {
+          let form = document.getElementById('can-text');
+          let showIcon = document.getElementById('view-icon')
+          let hideIcon = document.getElementById('hide-icon')
+          form.classList.remove('hidden')
+          showIcon.classList.add('hidden')
+          hideIcon.classList.remove('hidden')
+        }
+      }
+
+      const hideCanText = () => {
+        if (document.getElementById('can-text') && document.getElementById('view-icon') && document.getElementById('hide-icon')) {
+          let form = document.getElementById('can-text');
+          let showIcon = document.getElementById('view-icon')
+          let hideIcon = document.getElementById('hide-icon')
+          form.classList.add('hidden')
+          showIcon.classList.remove('hidden')
+          hideIcon.classList.add('hidden')
+        }
+      }
+
     return (<> 
         <div className='tile'>
             <div>
-                <span onClick={() => handleCopy()}>|C|</span>
-                <span onClick={() => updateCan()}> |*| </span>
-                <span onClick={() => deleteCan()}>|X|</span>
+
+                <div className='tile-controls'>
+                    <UncontrolledTooltip placement='top' target='clipboard-icon'>
+                        Copy can text to clipboard
+                    </UncontrolledTooltip>
+                    <BsClipboard id='clipboard-icon' onClick={() => handleCopy()}/>
+
+                    <UncontrolledTooltip placement='top' target='edit-icon'>
+                        Edit this can
+                    </UncontrolledTooltip>
+                    <BsFillGearFill id='edit-icon' onClick={() => updateCan()}/>
+
+                    <UncontrolledTooltip placement='top' target='delete-icon'>
+                        Delete this can
+                    </UncontrolledTooltip>
+                    <BsTrash id='delete-icon' onClick={() => deleteCan()}/>
+
+                    <UncontrolledTooltip placement='top' target='view-icon'>
+                        View can text
+                    </UncontrolledTooltip>
+                    <BsFillEyeFill id='view-icon' onClick={() => showCanText()}/>
+
+                    <UncontrolledTooltip placement='top' target='hide-icon'>
+                        Hide can text
+                    </UncontrolledTooltip>
+                    <BsFillEyeSlashFill id='hide-icon' className='hidden' onClick={() => hideCanText()}/>
+                </div>
+  
             </div>
-            {props.tileData.can_name}
-            <div className='tile-text'>{props.tileData.can_text}</div>
+            <p className='can-name'>{props.tileData.can_name}</p>
+            <div id='can-text' className='hidden'>{props.tileData.can_text}</div>
         </div>
 
         <Form onSubmit={updateCan}>
